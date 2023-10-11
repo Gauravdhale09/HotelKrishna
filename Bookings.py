231a44593fa668be8c880c3ca154d4411a74c03a
Gauravdhale09 <103017831+Gauravdhale09@users.noreply.github.com>
2023-10-11 23:42:37 +0530

    Update Bookings.py

--- Bookings.py
+++ Bookings.py
@@ -3,7 +3,7 @@ import streamlit as st
 class Booking:
     def __init__(self):
         st.markdown("""
-                                            <h1 style='text-align: center;'>Hotel <span style='color:red;'>Krishna</span></h1>
+                                            <h1 style='text-align: center;'>Hotel <span style='color:blue;'>Krishna</span></h1>
                                             """, unsafe_allow_html=True)
         st.subheader(f"Welcome")
         st.markdown("""
